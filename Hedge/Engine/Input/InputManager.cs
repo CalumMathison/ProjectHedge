9ff14dee68b9e5f9e3621c586ee32c06b91a9824
Calum Mathison <40406464@live.napier.ac.uk>
2020-06-25 19:39:41 +0100

    Created Project
    Added Input manager

---
+++ Hedge/Engine/Input/InputManager.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Engine.Input
+{
+    public class InputManager
+    {
+        public enum MouseButtons { Left, Right, Middle }
+        private static InputManager _instance;
+        private KeyboardState _currKb, _prevKb;
+        private MouseState _currMs, _prevMs;
+        private GamePadState _currGp, _prevGp;
+
+        public static InputManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new InputManager();
+                }
+                return _instance;
+            }
+        }
+
+        public InputManager()
+        {
+            _currKb = Keyboard.GetState();
+            _currMs = Mouse.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).IsConnected)
+            {
+                _currGp = GamePad.GetState(PlayerIndex.One);
+            }
+            
+        }
+
+        public void Update()
+        {
+            _prevKb = _currKb;
+            _prevMs = _currMs;
+
+            _currKb = Keyboard.GetState();
+            _currMs = Mouse.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).IsConnected)
+            {
+                _prevGp = _currGp;
+                _currGp = GamePad.GetState(PlayerIndex.One);
+            }
+        }
+
+        public bool IsMouseDown(MouseButtons b)
+        {
+            switch (b)
+            {
+                case MouseButtons.Left:
+                    return _currMs.LeftButton == ButtonState.Pressed;
+                case MouseButtons.Right:
+                    return _currMs.RightButton == ButtonState.Pressed;
+                case MouseButtons.Middle:
+                    return _currMs.MiddleButton == ButtonState.Pressed;
+            }
+            return false;
+        }
+
+        public bool IsMousePressed(MouseButtons b)
+        {
+            switch (b)
+            {
+                case MouseButtons.Left:
+                    return _currMs.LeftButton == ButtonState.Released &&
+                        _prevMs.LeftButton == ButtonState.Pressed;
+                case MouseButtons.Right:
+                    return _currMs.RightButton == ButtonState.Released &&
+                        _prevMs.RightButton == ButtonState.Pressed;
+                case MouseButtons.Middle:
+                    return _currMs.MiddleButton == ButtonState.Released &&
+                        _prevMs.MiddleButton == ButtonState.Pressed;
+            }
+            return false;
+        }
+
+        //public Vector2 GetMouseScreenPosition()
+        //{
+
+        //}
+
+        public bool IsKeyDown(Keys k)
+        {
+            return _currKb.IsKeyDown(k);
+        }
+
+        public bool IsKeyPressed(Keys k)
+        {
+            return _currKb.IsKeyUp(k) && _prevKb.IsKeyDown(k);
+        }
+
+        public bool IsButtonDown(Buttons b)
+        {
+            if (_currGp.IsConnected)
+            {
+                switch (b)
+                {
+                    case Buttons.DPadUp:
+                        return _currGp.DPad.Up == ButtonState.Pressed;
+                    case Buttons.DPadDown:
+                        return _currGp.DPad.Down == ButtonState.Pressed;
+                    case Buttons.DPadLeft:
+                        return _currGp.DPad.Left == ButtonState.Pressed;
+                    case Buttons.DPadRight:
+                        return _currGp.DPad.Right == ButtonState.Pressed;
+                    case Buttons.Start:
+                        return _currGp.Buttons.Start == ButtonState.Pressed;
+                    case Buttons.Back:
+                        return _currGp.Buttons.Back == ButtonState.Pressed;
+                    case Buttons.LeftShoulder:
+                        return _currGp.Buttons.LeftShoulder == ButtonState.Pressed;
+                    case Buttons.RightShoulder:
+                        return _currGp.Buttons.RightShoulder == ButtonState.Pressed;
+                    case Buttons.A:
+                        return _currGp.Buttons.A == ButtonState.Pressed;
+                    case Buttons.B:
+                        return _currGp.Buttons.B == ButtonState.Pressed;
+                    case Buttons.X:
+                        return _currGp.Buttons.X == ButtonState.Pressed;
+                    case Buttons.Y:
+                        return _currGp.Buttons.Y == ButtonState.Pressed;
+                    case Buttons.RightTrigger:
+                        return _currGp.Triggers.Right > 0.2f;
+                    case Buttons.LeftTrigger:
+                        return _currGp.Triggers.Left > 0.2f;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsButtonPressed(Buttons b)
+        {
+            if (_currGp.IsConnected)
+            {
+                switch (b)
+                {
+                    case Buttons.DPadUp:
+                        return _currGp.DPad.Up == ButtonState.Released &&
+                            _prevGp.DPad.Up == ButtonState.Pressed;
+                    case Buttons.DPadDown:
+                        return _currGp.DPad.Down == ButtonState.Released &&
+                            _prevGp.DPad.Down == ButtonState.Pressed;
+                    case Buttons.DPadLeft:
+                        return _currGp.DPad.Left == ButtonState.Released &&
+                            _prevGp.DPad.Left == ButtonState.Pressed;
+                    case Buttons.DPadRight:
+                        return _currGp.DPad.Right == ButtonState.Released &&
+                            _prevGp.DPad.Right == ButtonState.Pressed;
+                    case Buttons.Start:
+                        return _currGp.Buttons.Start == ButtonState.Released &&
+                            _prevGp.Buttons.Start == ButtonState.Pressed;
+                    case Buttons.Back:
+                        return _currGp.Buttons.Back == ButtonState.Released &&
+                            _prevGp.Buttons.Back == ButtonState.Pressed;
+                    case Buttons.LeftShoulder:
+                        return _currGp.Buttons.LeftShoulder == ButtonState.Released &&
+                            _prevGp.Buttons.LeftShoulder == ButtonState.Pressed;
+                    case Buttons.RightShoulder:
+                        return _currGp.Buttons.RightShoulder == ButtonState.Released &&
+                            _prevGp.Buttons.RightShoulder == ButtonState.Pressed;
+                    case Buttons.A:
+                        return _currGp.Buttons.A == ButtonState.Released &&
+                            _prevGp.Buttons.A == ButtonState.Pressed;
+                    case Buttons.B:
+                        return _currGp.Buttons.B == ButtonState.Released &&
+                            _prevGp.Buttons.B == ButtonState.Pressed;
+                    case Buttons.X:
+                        return _currGp.Buttons.X == ButtonState.Released &&
+                            _prevGp.Buttons.X == ButtonState.Pressed;
+                    case Buttons.Y:
+                        return _currGp.Buttons.Y == ButtonState.Released &&
+                             _prevGp.Buttons.Y == ButtonState.Pressed;
+                    case Buttons.RightTrigger:
+                        return _currGp.Triggers.Right < 0.2f &&
+                            _prevGp.Triggers.Right > 0.2f;
+                    case Buttons.LeftTrigger:
+                        return _currGp.Triggers.Left < 0.2f &&
+                            _prevGp.Triggers.Left > 0.2f;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Hedge/Hedge/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Engine.Input;
+
+namespace Hedge
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            InputManager.Instance.Update();
+
+            if (InputManager.Instance.IsKeyPressed(Keys.Escape) || 
+                InputManager.Instance.IsButtonPressed(Buttons.Start))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
